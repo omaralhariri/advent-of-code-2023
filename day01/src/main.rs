26fe1465090c2f711c5a7399ff005d055e0b7791
Omar Alhariri <o.alhariri@classera.com>
2023-12-16 16:20:09 +0300

    Finish day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,41 +1,84 @@
-use std::{fs, collections::BTreeMap};
+use std::{collections::BTreeMap, fs};
 
 fn main() {
-    let file = fs::read_to_string("input.txt")
-        .expect("Cannot read file");
+    let numbers: BTreeMap<&str, usize> = BTreeMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let nums_vec = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let file = fs::read_to_string("input.txt").expect("Cannot read file");
     let lines: Vec<&str> = file.lines().collect();
-    let mut lines_digits_idx: BTreeMap<usize, Vec<char>>  = BTreeMap::new();
+    let mut lines_digits: BTreeMap<usize, BTreeMap<usize, usize>> = BTreeMap::new();
 
     for (index, line) in lines.iter().enumerate() {
-        let chars: Vec<char> = line.chars().filter(|x: &char| {
-            return x.is_digit(10);
-        }).collect();
+        let mut all: BTreeMap<usize, usize> = BTreeMap::new();
+        for word in nums_vec.iter() {
+            let one: Vec<(usize, &str)> = line.match_indices(word).collect();
+            if one.len() > 0 {
+                for w in one.iter() {
+                    all.insert(w.0, numbers.get(w.1).unwrap().to_owned());
+                }
+            }
+        }
 
-        lines_digits_idx.entry(index).or_insert(chars);
+        if all.len() > 0 {
+            lines_digits.insert(index, all);
+        }
+
+        for (i, c) in line.chars().into_iter().enumerate() {
+            if c.is_digit(10) {
+                lines_digits
+                    .entry(index)
+                    .and_modify(|e| {
+                        e.entry(i)
+                            .or_insert(c.to_digit(10).unwrap().try_into().unwrap());
+                    })
+                    .or_insert(BTreeMap::from([(
+                        i,
+                        c.to_digit(10).unwrap().try_into().unwrap(),
+                    )]));
+            }
+        }
     }
 
     let mut digits: Vec<usize> = vec![];
-    
-    for map in lines_digits_idx.iter() {
-        let chars = map.1;
+
+    for mut map in lines_digits.values().cloned() {
+        let first = map.pop_first().unwrap_or((10, 10)).1;
+        let last = map.pop_last().unwrap_or((10, 10)).1;
+
         let mut two_digits: String = "".to_string();
-        if chars.len() == 0 {
+        if first == 10 && last == 10 {
             continue;
-        } else if chars.len() == 1 {
-            let first_digit: String = chars.first().unwrap().to_owned().to_string();
+        } else if last == 10 {
+            let first_digit: String = first.to_string();
             two_digits.push_str(first_digit.as_str());
             two_digits.push_str(first_digit.as_str());
+        } else if first == 10 {
+            let last_digit: String = last.to_string();
+            two_digits.push_str(last_digit.as_str());
+            two_digits.push_str(last_digit.as_str());
         } else {
-            let first_digit = chars.first().unwrap().to_string();
-            let last_digit = chars.last().unwrap().to_string();
+            let first_digit = first.to_string();
+            let last_digit = last.to_string();
             two_digits.push_str(first_digit.as_str());
             two_digits.push_str(last_digit.as_str());
         }
-        
+
         digits.push(two_digits.to_owned().parse::<usize>().unwrap());
     }
 
     let sum: usize = digits.iter().sum();
-    println!("Sum {}",sum); 
+    println!("Sum {}", sum);
 }
-
